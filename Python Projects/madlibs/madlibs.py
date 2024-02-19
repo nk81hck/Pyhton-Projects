a87e43bf8984761f709747f37e3fa28edff57b5c
Nishant Kumar Raghuvanshi <118392944+nk81hck@users.noreply.github.com>
2024-02-19 16:11:26 +0530

    Update madlibs.py

--- Python Projects/madlibs/madlibs.py
+++ Python Projects/madlibs/madlibs.py
@@ -1,6 +1,6 @@
 # string concatenation(aka how to put strings together)
 # suppose we want to create a string that says "subscribe to ____"
-# youtuber = "code with harry" #some strings variable
+# youtuber = "code with nkhck" #some strings variable
 #  a few ways to do this
 # print("subscribe to " + youtuber)
 # print("subscribe to {}".format(youtuber))
